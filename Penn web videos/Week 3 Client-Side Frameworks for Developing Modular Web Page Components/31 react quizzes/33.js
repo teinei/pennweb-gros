b2ce8c55526b4ac9864ade6298eab0d45363f4c1
zuid <zuid@zuids-MacBook-Pro.local>
2018-12-25 11:47:08 +0800

    rm renamed dirs

--- Penn web videos/Week 3 Client-Side Frameworks for Developing Modular Web Page Components/31 react quizzes/33.js
+++
@@ -1,4 +0,0 @@
-ReactDOM.render(
-    <div><MyText value="Hi there!" /></div>
-    document.getElementById('container')
-)
--- Penn web videos/Week 3 Client-Side Frameworks for Developing Modular Web Page Components/31 react quizzes/quiz3.3problem1.js
+++
@@ -1,3 +0,0 @@
-//buggy code
-//伴奏钢琴教程
-this.state = { value : this.state.value+1 }
